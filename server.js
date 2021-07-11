75d5eab8e3bffc3121ac303e298c1183a0aa2126
Szymon <sszulinski@gmail.com>
2021-07-11 14:14:20 +0200

    Subtask 31.4.1 && subtask 31.4.2

--- server.js
+++ server.js
@@ -2,12 +2,23 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
+const session = require('express-session');
+const passport = require('./config/passport');
 
 const app = express();
 
+// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
+// init session mechanism
+app.use(session({ secret: 'anything' }));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,13 +28,8 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-  res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-  res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
   res.status(404).render('notFound');
@@ -31,4 +37,4 @@ app.use('/', (req, res) => {
 
 app.listen('8000', () => {
   console.log('Server is running on port: 8000');
-});
+}); 
